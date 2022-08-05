5197b90b9e363eee962e602df8affeae423802d9
Sarah <sarahossai6@gmail.com>
2022-08-05 05:26:57 -0700

    Demo Page

---
+++ header&footer.js
@@ -0,0 +1,13 @@
+"strict mode";
+
+
+const toggleMenu = document.querySelector(".toggle_menu");
+const img = document.createElement("img");
+img.src
+
+toggleMenu.addEventListener("click", function() {
+    document.querySelector(".mobile-menu").classList.toggle("active");
+    toggleMenu.children.item(0).classList.toggle("show")
+    toggleMenu.children.item(1).classList.toggle("show")
+});
+Footer
