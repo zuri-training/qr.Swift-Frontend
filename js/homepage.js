63b78725938859f8b529727711b56a4d9849b511
Ekene C <clarencekene@outlook.com>
2022-08-09 11:15:32 +0100

    Mobile and tablet view responsiveness complete Edited colour scheme Changed testimonial profile picture Changed border radius for buttons Added JS for carousel Added Header&Footer HTML, CSS and JS to respective homepage file.

---
+++ js/homepage.js
@@ -0,0 +1,42 @@
+"strict mode";
+
+
+const toggleMenu = document.querySelector(".toggle_menu");
+const img = document.createElement("img");
+img.src
+
+toggleMenu.addEventListener("click", function () {
+    document.querySelector(".mobile-menu").classList.toggle("active");
+    toggleMenu.children.item(0).classList.toggle("show")
+    toggleMenu.children.item(1).classList.toggle("show")
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail/dot controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("share");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "";
+        // slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
