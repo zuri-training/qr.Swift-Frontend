9d91c999222e9c684bd13eea971fada687ae1670
psalmuelle <bjoestores@gmail.com>
2022-08-15 14:27:30 +0100

    4tz

--- javascript/text.js
+++ javascript/text.js
@@ -1,37 +1,70 @@
-"strict mode";
 
-const login= ()=>{
 
-    loginForm.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    loginDetails.append("username", document.getElementById("login_username").value)
-    loginDetails.append("password", document.getElementById("login_password").value)
-    
-    fetch(api_endpoint,{
-        method: "POST",
-        body: loginDetails,
+const toggleMenu = document.querySelector(".toggle_menu");
+
+
+toggleMenu.addEventListener("click", function() {
+    document.querySelector(".mobile-menu").classList.toggle("active");
+    toggleMenu.children.item(0).classList.toggle("show")
+    toggleMenu.children.item(1).classList.toggle("show")
+});
+
+const downloadbtn = document.querySelector("#print")
+
+let imageLink;
+document.getElementById('generate').addEventListener('click', addPost);
+
+
+
+
+function addPost(e){
+    e.preventDefault();
+    let url = document.getElementById('url').value;
+ 
+
+    fetch('https://qrswiftapp.herokuapp.com/api/create-qr/', {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json, text/Plain, */*',
+            'Content-type':'application/json'
+        },
+        body: JSON.stringify({qr_type: "URL", url:url,userId : 12})
+
     })
-    .then(res => res.json())
-    .then(data => {
-    
-        userData = data
-        console.log(userData.id)
-    
-    
-       
-                        if(data.token){
-                            
-                            
-                            window.location.href="dashboard.html?data="+JSON.stringify(userData.id)
-                            
-    
-                        }else{
-                            alert("Login Failed")
-                        }
+    .then((res) => res.json())
+    .then((data) => {
+        document.querySelector(".code").src = "https://qrswiftapp.herokuapp.com"+ data.qr.qr_code
+        imageLink = "https://qrswiftapp.herokuapp.com"+ data.qr.qr_code
+        console.log(data, data.qr.qr_code)})
+}
+
+downloadbtn.addEventListener("click", ()=>{
+    const element = document.createElement("a")
+    element.setAttribute("href", imageLink)
+    element.setAttribute("download", "image.png" )
+    document.body.appendChild(element)
+    element.click()
+})
+
+
+const shareData = {
+    title: 'QR',
+    text: 'My Qr Image',
+    url: imageLink
+  }
+  
+  const btn = document.querySelector('.sharebtn');
+ // const resultPara = document.querySelector('.result');
+  
+  // Share must be triggered by "user activation"
+  btn.addEventListener('click', async () => {
+    try {
+      await navigator.share(shareData);
+     // resultPara.textContent = 'MDN shared successfully';
+
+    } catch (err) {
+      //resultPara.textContent = `Error: ${err}`;
+      console.log(err)
     }
-        )
-    .catch(err => console.log(err))
-    })
-    }
-    
-    login()
+  });
+  
